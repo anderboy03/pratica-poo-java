ee47c9e735ea6945606fabeca9b00cbef66d6b99
Anderson Clayton Ribas <andersex03@gmail.com>
2024-10-10 11:44:23 -0300

    commit aula 6 controle
    aula pratica orientação objeto usando interface e encapsulamento

---
+++ aula06b/src/aula06b/Controlador.java
@@ -0,0 +1,18 @@
+
+package aula06b;
+
+
+public interface Controlador {
+    
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abriMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void pausar();
+    
+}
